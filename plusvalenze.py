5538b5625195c757671893df06df8d475da8b092
Corby25 <tommaso.diegoli@gmail.com>
2022-06-02 17:32:53 +0200

    completo

--- plusvalenze.py
+++ plusvalenze.py
@@ -1,5 +1,11 @@
-import web3
+from web3 import Web3, EthereumTesterProvider
 from openpyxl import load_workbook
+from datetime import datetime
+import requests, json
+
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/6b46939a901e4fb798dc0aa82ce92781'))
+w3 = Web3(Web3.WebsocketProvider('wss://mainnet.infura.io/ws/v3/6b46939a901e4fb798dc0aa82ce92781'))
+print(w3.isConnected())
 
 with open("txns.txt") as file:
     list_hashes = file.readlines()
@@ -9,12 +15,20 @@ wb = load_workbook(workbook_name)
 page = wb.active
 
 row_to_append = []
+
 for hash in list_hashes:
-    row_to_append.append()
+    txn = w3.eth.getTransaction(hash.strip())
+    timestamp = dict(w3.eth.get_block(txn.blockNumber))["timestamp"]
 
-new_companies = [['name3','address3','tel3','web3'], ['name4','address4','tel4','web4']]
+    response_price = requests.get(
+        "https://min-api.cryptocompare.com/data/v2/histoday?fsym=ETH&tsym=EUR&limit=1&toTs=" + str(
+            timestamp + 86400) + "&api_key=b845ea0a472a48c64593408ed0566c50f311d98ec840d08204f6c62132ae611f")
+    prices = json.loads(response_price.text)
+    eth_eur = prices["Data"]["Data"][1]["low"]
 
-for info in new_companies:
-    page.append(info)
+    row_to_append.append([txn.hash, datetime.fromtimestamp(timestamp), eth_eur, "", w3.fromWei(int(txn.value), 'ether')])
+
+for row in row_to_append:
+    page.append(row)
 
 wb.save(filename=workbook_name)
